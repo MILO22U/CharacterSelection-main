1e52efa85af9922642a7e727311bd65c6e6b27a2
MILO22U <38486178+MILO22U@users.noreply.github.com>
2022-10-03 08:03:30 +0530

    Update 3

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -7,14 +7,24 @@ public class TankController
 
    private TankView tankView;
 
+   private Rigidbody rb;
+
    public TankController(TankModel _tankModel , TankView _tankView)
    {
     tankModel  = _tankModel;
-    tankView   = _tankView;
+    tankView   = GameObject.Instantiate<TankView>(_tankView);
+    rb = tankView.GetRigidbody();
     tankModel.SetTankController(this);
     tankView.SetTankController(this);
+   }
 
-    GameObject.Instantiate(tankView.gameObject);
+   public void Move(float movement, float movementSpeed)
+   {
+          rb.velocity = tankView.transform.forward * movement * movementSpeed;
+   }
+
+   public void Rotate(float rotate, float rotateSpeed)
+   {
     
    }
 
--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -6,6 +6,13 @@ public class TankView : MonoBehaviour
 {
 
     private TankController tankController;
+
+    private float movement;
+
+    private float rotation;
+
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +22,25 @@ public class TankView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+
+        if (movement!=0)
+        tankController.Move(movement, 30);
+    }
+
+    private void Movement()
+    {
+        movement = Input.GetAxis("Vertical");
+        rotation = Input.GetAxis("Horizontal");
     }
 
      public void SetTankController(TankController _tankController)
     {
         tankController = _tankController;  
+    }
+
+    public Rigidbody GetRigidbody()
+    {
+        return rb;
     }
 }
